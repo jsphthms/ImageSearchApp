a6dcf319442bcda79bfd68b00cfc7353973ab4b2
jsphthms <josephreverethomas@gmail.com>
2020-04-29 14:33:12 -0700

    Styling

--- src/ImageView/ImageView.js
+++ src/ImageView/ImageView.js
@@ -5,12 +5,20 @@ import { Link } from 'react-router-dom'
 function ImageView(props) {
     const { largeImageURL, tags, user, pageURL } = props.location.state.pic
     return (
-        <div>
-            <button><Link to='/'>Home</Link></button>
-            <img src={largeImageURL} alt={tags} srcset="" />
-            <p>&copy;Pixabay</p>
-            <h4>Credit: <span>{user}</span> </h4>
-            <h4>Download: <span><a href={pageURL} target="_blank" rel="noopener noreferrer" > Url</a></span></h4>
+        <div className='container'>
+            <div className='row'>
+
+                <div className='imageView__container'>
+                    <button><Link to='/'>Home</Link></button>
+                    <img className='imageView__img img-responsive' src={largeImageURL} alt={tags} srcset="" />
+                    <div className="imageView__copyright">
+                        <p>&copy;Pixabay</p>
+                    </div>
+                    <h4>Credit: <span>{user}</span> </h4>
+                    <h4>Download: <span><a href={pageURL} target="_blank" rel="noopener noreferrer" > URL</a></span></h4>
+                </div >
+            </div >
+
         </div >
     )
 }
